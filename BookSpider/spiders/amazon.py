ee8a3b4cb77567f6e9c99e1dee62a73f40f88406
xchaoinfo <xchaoinfo@qq.com>
2017-06-01 22:21:24 +0800

    更改抓取方式，提高抓取效率

--- BookSpider/spiders/amazon.py
+++ BookSpider/spiders/amazon.py
@@ -7,10 +7,20 @@ from scrapy.spiders import CrawlSpider, Rule
 import re
 
 
+# def get_start_urls_from_txt(fn="start_url.txt"):
+#     with open(fn) as fr:
+#         url_list = [f.strip() for f in fr if f.strip()]
+
+#     return url_list
+
+
 class AmazonSpider(CrawlSpider):
     name = 'amazon'
     allowed_domains = ['amazon.cn']
-    start_urls = ["https://www.amazon.cn/b/ref=sr_aj?node=658394051&ajr=0"]
+    # start_urls = ["https://www.amazon.cn/b/ref=sr_aj?node=658394051&ajr=0"]
+    # start_urls = get_start_urls_from_txt()
+    start_urls = ("https://www.amazon.cn/gp/search/other/ref=sr_sa_p_lbr_one_browse-bin?rh=n%3A658390051%2Cn%3A%21658391051%2Cn%3A658394051%2Cp_6%3AA1AJ19PSB66TGU%2Cp_n_binding_browse-bin%3A2038564051&bbn=658394051&pickerToList=lbr_one_browse-bin&ie=UTF8&qid=1496321636",
+        "https://www.amazon.cn/gp/search/other/ref=sr_sa_p_lbr_one_browse-bin?rh=n%3A658390051%2Cn%3A%21658391051%2Cn%3A658394051%2Cp_n_binding_browse-bin%3A2038565051&bbn=658394051&pickerToList=lbr_one_browse-bin&ie=UTF8&qid=1496321894")
     url_xpath = '//a[contains(@class, "s-color-twister-title-link")]'
     author_xpath = '//div[@id="refinementList"]'
     nextPage_xpath = '//a[@id="pagnNextLink"]'
@@ -19,20 +29,49 @@ class AmazonSpider(CrawlSpider):
         # Rule(LinkExtractor(restrict_xpaths=(author_xpath,)), follow=True),
         # Rule(LinkExtractor(restrict_xpaths=(book_class_xpath,)), follow=True),
         # Rule(LinkExtractor(allow=("b/ref.*",), restrict_xpaths=(book_class_xpath,)), follow=True),
-        # Rule(LinkExtractor(restrict_xpaths=(nextPage_xpath,)), follow=True),
-        Rule(LinkExtractor(deny=("b/ref.*",), restrict_xpaths=(url_xpath,)), callback="parse_item"),
+        Rule(LinkExtractor(restrict_xpaths=(nextPage_xpath,)), follow=True, callback="parse_search_result"),
+        # Rule(LinkExtractor(deny=("b/ref.*",), restrict_xpaths=(url_xpath,)), callback="parse_item"),
     )
 
-    # def start_requests(self):
-    #     for url in self.start_urls:
-    #         yield scrapy.Request(url=url, callback=self.parse_starturls)
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse_starturls)
 
     def parse_starturls(self, response):
-        url_list = response.xpath('//div[contains(@class, "browseBox")]/ul[2]/li/a/@href').extract()
+        # url_list = response.xpath('//div[@class="categoryRefinementsSection"]/ul/li/a/@href').extract()[1:]
+        # url_list = response.xpath('//div[contains(@class, "browseBox")]/ul[2]/li/a/@href').extract()
+        url_list = response.xpath('//li/span[@class="a-list-item"]/a/@href').extract()
         home_url = "https://www.amazon.cn"
         url_list = [home_url + u for u in url_list]
         for url in url_list:
-            yield scrapy.Request(url)
+            yield scrapy.Request(url, callback=self.parse_search_result)
+
+    def parse_search_result(self, response):
+        result = response.xpath('//div[contains(@class, "a-fixed-left-grid-col a-col-right")]')
+        for res in result:
+            name = res.xpath('div[@class="a-row a-spacing-small"]/div/a/@title').extract()[0]
+            url = res.xpath('div[@class="a-row a-spacing-small"]/div/a/@href').extract()[0]
+
+            packed = res.xpath('div[@class="a-row"]/div[@class="a-column a-span7"]/div/a/h3/text()').extract()[0]
+            price = res.xpath('div[@class="a-row"]/div[@class="a-column a-span7"]/div[2]/a/span/text()').extract()[0]
+            if price:
+                price = float(price.replace("￥", ''))
+            else:
+                price = 0
+            comments_list = res.xpath('div[@class="a-row"]/div[@class="a-column a-span5 a-span-last"]/div/a/text()').re(r'\d+')
+            comments_num = "".join(comments_list)
+            if comments_num:
+                comments_num = int(comments_num)
+            else:
+                comments_num = 0
+
+            AmazonItem = AmazonSpiderItem()
+            AmazonItem['url'] = url
+            AmazonItem['name'] = name
+            AmazonItem['packed'] = packed
+            AmazonItem['comments_num'] = comments_num
+            AmazonItem['price'] = price
+            yield AmazonItem
 
     def parse_item(self, response):
         """[summary]
